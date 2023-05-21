48e5d0510e39d23d810858b3d64f07248e963cc7
Lucas Rocha <lr359508@gmail.com>
2023-05-20 21:45:40 -0300

    Criação do ambiente.

---
+++ scripts/jogo.js
@@ -0,0 +1,18 @@
+function ger_num(){
+    return Math.floor(Math.random()*6)+1
+}
+
+let ctx = document.querySelector('canvas#canvas').getContext('2d')
+
+//Famoso Canvas
+
+//Cria um circulo.
+/* ctx.beginPath()
+ctx.strokeStyle = "rgb(200,0,0)"
+ctx.arc(50,40,40,0,Math.PI,false)
+ctx.closePath()
+ctx.fill() */
+
+//Cria um retângulo.
+/* ctx.strokeRect(100,50,200,100)
+ */
